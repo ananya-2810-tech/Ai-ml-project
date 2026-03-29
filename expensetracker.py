f955db8b7afd91de219a0f1cc9d67603f17e68da
ananya-2810-tech <ananyaasharma.28@gmail.com>
2026-03-29 15:07:13 +0530

    Update expensetracker.py

--- expensetracker.py
+++ expensetracker.py
@@ -1,49 +1,55 @@
-#Personal Expense Tracker
+#Personal Expense Tracker 
+from ml_model import predict_category
 
 expenses = []
 
 def add_expense():
-  name = input("Enter expense name: ")
-  amount = float(input("Enter amount: "))
-  expense = {"name": name, "amount": amount}
-  expenses.append(expense)
+    name = input("Enter expense name: ")
+    amount = float(input("Enter amount: "))
 
-print("Expenses added successfully!\n")
+    category = predict_category(name)
+
+    expenses.append({
+        "name": name,
+        "amount": amount,
+        "category": category
+    })
+
+    print("Predicted Category:", category)
+    print("Expense added successfully\n")
+
 
 def view_expenses():
-  if not expenses:
-    print("No expenses recorded. \n")
-    return
+    if not expenses:
+        print("No expenses recorded\n")
+        return
 
-  print("\nYour Expenses:")
-  for expense in expenses:
-    print(expense["name"], ":" , expense["amount"])
-    print()
+    for e in expenses:
+        print(e["name"], "-", e["category"], "-", e["amount"])
+
 
 def total_expense():
-  total = sum(expense{"amount"} for expense in expenses)
-  print("Total Expense:", total, "\n")
+    total = sum(e["amount"] for e in expenses)
+    print("Total Expense:", total)
 
-def main():
-  while True:
-    print("Personal Expense Tracker")
-    print("1. Add Expenses")
+
+while True:
+    print("\nPersonal Expense Tracker (ML Based)")
+    print("1. Add Expense")
     print("2. View Expenses")
-    print("3. Show Total Expenses")
+    print("3. Show Total Expense")
     print("4. Exit")
 
-    choice = input("Enter your choice : ")
+    choice = input("Enter your choice: ")
 
     if choice == "1":
-      add_expense()
+        add_expense()
     elif choice == "2":
-      view_expense()
+        view_expenses()
     elif choice == "3":
-      total_expense()
+        total_expense()
     elif choice == "4":
-      print("Exiting program.")
-      break
+        print("Exiting program")
+        break
     else:
-      print("Invalid choice\n")
-
-main()
+        print("Invalid choice")
